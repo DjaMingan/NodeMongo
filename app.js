aec493e9d4fbd1159bf1b2324a626d48951f1589
DjaMingan <daminian@swissonline.ch>
2020-08-09 16:07:54 +0200

    Express Session Part 2

--- app.js
+++ app.js
@@ -43,45 +43,26 @@ app.use(session({
   resave:false,
   store: new FileStore()
 }));
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 
 function auth(req,res,next) {
     console.log(req.session);
 
     if(!req.session.user) {
-        var authHeader = req.headers.authorization;
-
-        if(!authHeader){
-          var err = new Error('You are not authenticated');
-          res.setHeader('WWW-Authenticate', 'Basic');
-          err.status = 401;
-          return next(err);
-      }
-
-      var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-
-      var user = auth[0];
-      var password = auth[1];
-
-       if(user === 'admin' && password ==="password") {
-          req.session.user = 'admin';
-          next();
-       }
-       else {
          var err = new Error('You are not authenticated');
-         res.setHeader('WWW-Authenticate', 'Basic');
-         err.status = 401;
+         err.status = 403;
          return next(err);
        }
-    }
     else {
-      if(req.session.user === 'admin') {
-          console.log('req.session: ',req.session);
+      if(req.session.user === 'authenticated') {
           next();
       }
       else {
         var err = new Error('You are not authenticated');
-        err.status = 401;
-        next(err);
+        err.status = 403;
+        return next(err);
       }
     }
   };
