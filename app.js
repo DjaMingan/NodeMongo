5e61857d423177fe1c8707b538f24e18c6b8a663
DjaMingan <daminian@swissonline.ch>
2020-08-10 17:53:02 +0200

    Passport JWT

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
+var config = require('./config');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,7 +21,7 @@ const dishes = require('./models/dishes');
 const promotions = require('./models/promotions');
 const leaders = require('./models/leaders');
 
-const url = 'mongodb://localhost:27017/conFusion';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
@@ -36,47 +39,17 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser('12345-67890-09876-54321'));
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave:false,
-  store: new FileStore()
-}));
+
+app.use(passport.initialize());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-
-function auth(req,res,next) {
-    console.log(req.session);
-
-    if(!req.session.user) {
-         var err = new Error('You are not authenticated');
-         err.status = 403;
-         return next(err);
-       }
-    else {
-      if(req.session.user === 'authenticated') {
-          next();
-      }
-      else {
-        var err = new Error('You are not authenticated');
-        err.status = 403;
-        return next(err);
-      }
-    }
-  };
-
-app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
